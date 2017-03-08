7cf4a2ff6c92f463acc4e1795a9ae481428f5bce
Yoga Setiawan <yogainformatika@gmail.com>
2017-03-09 04:36:12 +0700

    New

--- AmalanHarian/app/src/main/java/com/arioki/amalanharian/Home.java
+++ AmalanHarian/app/src/main/java/com/arioki/amalanharian/Home.java
@@ -3,11 +3,44 @@ package com.arioki.amalanharian;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Home extends AppCompatActivity {
+
+    PieChart pieChart ;
+    ArrayList<Entry> entries ;
+    ArrayList<String> PieEntryLabels ;
+    PieDataSet pieDataSet ;
+    PieData pieData ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        pieChart = (PieChart) findViewById(R.id.chart);
+
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(18.5f, "Green"));
+        entries.add(new PieEntry(26.7f, "Yellow"));
+        entries.add(new PieEntry(24.0f, "Red"));
+        entries.add(new PieEntry(30.8f, "Blue"));
+
+        PieDataSet set = new PieDataSet(entries, "Election Results");
+        PieData data = new PieData(set);
+        set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        pieChart.setData(data);
+        pieChart.invalidate(); // refresh
+
     }
 }
